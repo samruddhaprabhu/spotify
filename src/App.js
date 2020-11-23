750bbc07ad0d6f202a0e72518564aeee9393a0f1
DungeonMaster976 <38967050+DungeonMaster976@users.noreply.github.com>
2020-11-23 17:27:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Login from './Login';
-import { getTokenFromResponse } from './spotify';
-import SpotifyWebApi from 'spotify-web-api-js';
-import Player from './Player';
-import {userDataLayerValue} from './DataLayer';
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
 
-const spotify = new SpotifyWebApi();
+const s = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState(null);
-  const [{}, dispatch] = userDataLayerValue();
+  const [{ token }, dispatch] = useStateValue();
 
   useEffect(() => {
+    // Set token
     const hash = getTokenFromResponse();
     window.location.hash = "";
-    const _token = hash.access_token;
+    let _token = hash.access_token;
 
-    if(_token) {
-      setToken(_token);
+    if (_token) {
+      s.setAccessToken(_token);
 
       dispatch({
-        type: 'SET_USER',
-        user: user,
-        
-      })
+        type: "SET_TOKEN",
+        token: _token,
+      });
 
-      spotify.setAccessToken(_token);
+      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
 
-      spotify.getMe().then(user => {
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
 
-      })
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
     }
+  }, [token, dispatch]);
 
-
-  }, []);
   return (
     <div className="app">
-      {
-        token ? (
-          <Player/>
-        ) : (
-          <Login/>
-        )
-      }
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -21,4 +21,6 @@ export const getTokenFromResponse = () => {
     }, {});
 };
 
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
